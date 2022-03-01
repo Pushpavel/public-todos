0ccc7842be9532127373269ef214065d59fa49da
pushpavel <jpushpavel@gmail.com>
2022-03-01 11:03:17 +0530

    add firestore

--- src/components/todo/AddTodo.svelte
+++ src/components/todo/AddTodo.svelte
@@ -1,8 +1,28 @@
 <script lang="ts">
     import {autoresize} from 'svelte-textarea-autoresize'
+    import {getFirestore, addDoc, collection, serverTimestamp} from "firebase/firestore"
 
+    let value: string
+
+    function addTodo() {
+        const db = getFirestore()
+        addDoc(collection(db, "Todos"), {
+            text: value,
+            createdAt: serverTimestamp()
+        })
+    }
+
+    // todo: validate textarea
+    // todo: focus on textarea when clicked on div
 </script>
 
 <div class="card focus-within:outline outline-2">
-    <textarea class="w-full h-full align-top resize-none outline-none" use:autoresize>asdads</textarea>
+    <textarea class="w-full h-full align-top resize-none outline-none"
+              bind:value
+              use:autoresize
+              on:keydown={(e)=>{
+                  if(e.altKey && e.keyCode === 13)
+                      addTodo()
+              }}
+    ></textarea>
 </div>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,9 @@
 <script>
-  import "../app.css";
+    import "../app.css";
+    import {firebaseConfig} from "../env";
+    import {initializeApp} from "firebase/app"
+
+    initializeApp(firebaseConfig)
 </script>
 
-<slot />
+<slot/>
