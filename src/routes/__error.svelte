3407746ec732b7fe6f8135a47dfc60e89838f2da
pushpavel <jpushpavel@gmail.com>
2022-03-03 20:05:06 +0530

    404 page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,13 @@
+<script>
+    import {slide} from 'svelte/transition'
+</script>
+<div class="card" in:slide out:slide>Whoa! you reached this page 🤔. It inevitably means you did something you don't know... or you are just
+    lost. anyway
+    try
+    <a href="/" class="text-blue-500">
+        this
+        <span class="material-icons text-sm">link</span>
+    </a>
+    <br>
+    btw. Some call this a <span class="font-bold">404</span>
+</div>
