0bc53380e74b6404f0498dc24b97748fc2205d2c
pushpavel <jpushpavel@gmail.com>
2022-03-03 17:24:26 +0530

    add header styles

---
+++ src/components/Header.svelte
@@ -0,0 +1,84 @@
+<script>
+    import GithubLogo from "./GithubLogo.svelte";
+    import {slide} from 'svelte/transition'
+
+    let toggle = true
+</script>
+
+<header class="grid sm:grid-flow-col-dense mb-8">
+    <h1 class="col-span-2 text-4xl sm:text-5xl text-center sm:text-left">Public Todos</h1>
+    <div class="flex items-center col-span-2 sm:col-span-1 justify-around sm:justify-end">
+        <button class="flex items-center text-gray-200 hover:bg-white/[0.12] active:text-white/[0.7] hover:text-white  px-3 py-2 rounded-md text-sm font-medium"
+                on:click={()=>window.open('https://github.com/Pushpavel/public-todos', '_blank')}
+        >
+            <GithubLogo/>
+            <span class="ml-2">Github</span>
+            <sup class="material-icons text-base ml-1">open_in_new</sup>
+        </button>
+        <button class={`flex items-center text-gray-200 hover:bg-white/[0.12] active:text-white/[0.7] hover:text-white  px-3 py-2 rounded-md text-sm font-medium
+                    transition-all duration-200 ease-in-out
+                        ${toggle?"bg-white/[0.12] ml-4":""}`}
+                on:click={()=>toggle=!toggle}
+        >
+            <span class="material-icons text-3xl">lightbulb</span>
+            <span class="ml-2">How</span>
+            {#if toggle}
+                <span class="material-icons" in:slide>expand_more</span>
+            {/if}
+        </button>
+    </div>
+</header>
+
+{#if toggle}
+    <main class="card mb-8 !bg-black/[0.5] text-white !p-8" in:slide out:slide={{duration:1000}}>
+        <p>Open todo board to look up things todo when bored and add things todo when bored.</p>
+        <br>
+        <h4 class="mb-4">
+            <i class="material-icons">add_circle_outline</i>
+            <span class="ml-2 ">Create Todos</span>
+        </h4>
+        <p>The Todos you create are publicly available to everyone. They can't be removed once created. Go on add
+            something todo when bored.</p>
+
+        <h4 class="mt-8 mb-4">
+            <i class="material-icons">add_task</i>
+            <span class="ml-2 ">Want Todo</span>
+        </h4>
+        <p>
+            Explore
+            <span class="block">
+                <span class="material-icons">whatshot</span>
+                <span class="ml-2">Hot</span>
+            </span>
+            <span class="block">
+                <span class="material-icons">trending_up</span>
+                <span class="ml-2">Most Wanted</span>
+            </span>
+            Sections and mark todos you want todo. They show up in the
+            <span class="block">
+                <span class="material-icons">task_alt</span>
+                <span class="ml-2">Want Todo</span>
+            </span>
+        </p>
+
+    </main>
+{/if}
+
+<style>
+    h1 {
+        color: white;
+        font-family: "Open Sans ExtraBold", sans-serif;
+    }
+
+    h4 {
+        @apply flex items-center text-xl;
+    }
+
+    p {
+        @apply text-white/[0.8];
+    }
+
+    .block {
+        @apply inline-flex items-center border border-white/[0.5] rounded-lg px-2 pr-4 py-1;
+    }
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,7 +7,7 @@
     import {setContext} from "svelte";
     import NavBar from "../components/NavBar.svelte";
     import NavLink from "../components/NavLink.svelte";
-    import GithubLogo from "../components/GithubLogo.svelte";
+    import Header from "../components/Header.svelte";
 
     initializeApp(firebaseConfig);
     setContext(
@@ -16,25 +16,7 @@
     );
 </script>
 
-
-<div class="flex items-center text-yellow-500">
-    <h1>Public Todos</h1>
-    <span class="flex items-center ml-4">
-        <a href="https://github.com/Pushpavel/public-todos">
-            <img alt="GitHub Repo stars" src="https://img.shields.io/github/stars/Pushpavel/public-todos?style=social">
-        </a>
-    </span>
-    <span class="flex-1"></span>
-    <button class="flex items-center p-4 hover:text-white">
-        <GithubLogo/>
-        <span class="ml-2">Github</span>
-    </button>
-    <button class="flex items-center p-4 hover:text-white">
-        <span class="material-icons text-4xl">lightbulb</span>
-        <span class="ml-2">How</span>
-    </button>
-</div>
-
+<Header/>
 <NavBar class="mb-8">
     <NavLink href="/" class="flex items-center justify-center">
         <span class="material-icons mr-2">task_alt</span>
@@ -52,12 +34,3 @@
 <div class="relative">
     <slot/>
 </div>
-
-<style>
-    h1 {
-        font-size: 4rem;
-        color: white;
-        font-family: "Open Sans ExtraBold", sans-serif;
-        margin-bottom: 1rem;
-    }
-</style>
